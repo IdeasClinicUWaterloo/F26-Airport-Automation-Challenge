1b2dd158f5fd4544fb3bbf883a2eac9d18870cc0
AahanBhatt123 <a88bhatt@uwaterloo.ca>
2026-06-12 16:16:22 -0400

    test

--- flight-routing/message_parser.py
+++ flight-routing/message_parser.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 
 class MessageParser:
 
@@ -8,12 +9,45 @@ class MessageParser:
 
     
     def get_info(self):
-        return self.data["messages"]
+
+        info = {
+            "flight": None,
+            "route": None,
+            "position": None,
+            "altitude": None,
+            "speed": None,
+            "current_waypoint": None,
+            "next_waypoint": None,
+            "eta": None
+        }
+        
+        info["flight"] = self.data.get("flight_id")
+
+        message_type = self.data.get("type")
+
+        messages = self.data.get("messages", [])
+
+        for message in messages:
+            if message_type == "route_update":
+                info["route"] = message.get("route")
+            
+            if message_type == "state":
+                info["position"] = (message.get("lat"), message.get("lon"))
+                info["altitude"] = message.get("altitude")
+                info["speed"] = message.get("speed")
+
+            if message_type == "waypoint_report":
+                info["current_waypoint"] = message.get("current_waypoint")
+                info["next_waypoint"] = message.get("next_waypoint")
+                info["eta"] = message.get("eta")
+        
+        return info
+
+
 
     def display_info(self):
-        for message in self.get_messages():
-            print(message)
+        pprint(self.get_info())
 
 
 parser = MessageParser()
-parser.display_messages()
+parser.display_info()
