9e977dd4daceed1ebc36a64723bf39dcdc6dc203
AahanBhatt123 <a88bhatt@uwaterloo.ca>
2026-06-19 15:55:05 -0400

    Add bearing calculation.

--- flight-routing/dead_reckoning.py
+++ flight-routing/dead_reckoning.py
@@ -26,6 +26,36 @@ class DeadReckoning:
         
         dist = earth_radius * c
 
-        return dist
+        return dist/1000 #return in km
+    
+    def find_bearing(self, lat1, lon1, lat2, lon2):
+
+        phi1 = math.radians(lat1)
+        lambda1 = math.radians(lon1)
+        phi2 = math.radians(lat2)
+        lambda2 = math.radians(lon2)
+
+        y = math.sin(lambda2 - lambda1) * math.cos(phi2)
+
+        x = ( math.cos(phi1) * math.sin(phi2)
+            - math.sin(phi1) * math.cos(phi2) * math.cos(lambda2 - lambda1)
+        )
+
+        theta_rad = math.atan2(y, x)
+        theta = math.degrees(theta_rad)
+
+        return theta
+        
+    def print_distance(self, lat1, lon1, lat2, lon2):
+        dist = self.find_distance(lat1, lon1, lat2, lon2)
+        print(f"Distance between ({lat1}, {lon1}) and ({lat2}, {lon2}) is {dist} km.")
+    
+    def print_bearing(self, lat1, lon1, lat2, lon2):
+        bearing = self.find_bearing(lat1, lon1, lat2, lon2)
+        print(f"Bearing from ({lat1}, {lon1}) to ({lat2}, {lon2}) is {bearing} degrees")
+
+dr = DeadReckoning()
+dr.print_distance(43.6777, -79.6248, 40.6413, -73.7781)
+dr.print_bearing(43.6777, -79.6248, 40.6413, -73.7781)
 
         
